f6e05e45f90eb7b780406f57959f8759e97a4da5
henrymazza <fabio.mazarotto@me.com>
2011-08-03 16:55:07 -0300

    Added ssh_known_hosts to base role.

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,6 @@
 name "base"
 description "The base role for systems that serve HTTP traffic, Rails ready."
-run_list "recipe[hostname]", "recipe[users::sysadmins]", "recipe[sudo]", "recipe[postfix]"
+run_list "recipe[hostname]", "recipe[users::sysadmins]", "recipe[sudo]", "recipe[postfix]", "recipe[ssh_known_hosts]"
 default_attributes "authorization" => {
   "sudo" => {
     "groups" => ["admin", "wheel", "sysadmin"],
