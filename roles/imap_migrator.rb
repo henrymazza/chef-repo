c821499e5c5242a1763f081c91b63696c1556407
henrymazza <fabio.mazarotto@me.com>
2012-02-24 15:57:30 -0200

    imap_migrator application role

---
+++ roles/imap_migrator.rb
@@ -0,0 +1,16 @@
+name "imap_migrator"
+	run_list "recipe[application]"
+ 
+	override_attributes :apps => { 
+	  :imap_migrator=> { 
+	    :production => { 
+	      :run_migrations => false,
+	      :force => false
+	    },
+	    :staging => {
+	      :run_migrations => false,
+	      :force => false
+	    }
+	  }
+	}
+
