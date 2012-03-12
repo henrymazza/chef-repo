402181e0fc565ff0038aeb8239e3615c5d3964f7
henrymazza <fabio.mazarotto@me.com>
2012-03-12 12:43:32 -0300

    new resque recipe

--- cookbooks/resque/metadata.rb
+++ cookbooks/resque/metadata.rb
@@ -1,7 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures resque"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends          "runit"
+maintainer        "Paper Cavalier"
+maintainer_email  "code@papercavalier.com"
+license           "Apache 2.0"
+description       "Installs resque from rubygems and manages it via bluepill"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
+version           "0.1.0"
+
+recipe "resque::default", "Installs resque from rubygems and manages it via bluepill, defaults to Capistrano setup. REE aware."
--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: resque
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
 #
-# All rights reserved - Do Not Redistribute
+# We're not setting up Resque this way
+# because our projects use different rubies and specify their gems via Bundler
 #
+# if ['solo', 'util'].include?(node[:instance_role])
+#   %w[bluepill resque redis redis-namespace yajl-ruby].each do |install_gem|
+#     if node.run_state[:seen_recipes].has_key?("ruby_enterprise")
+#       ree_gem install_gem
+#     else
+#       gem_package install_gem
+#     end
+#   end
+# end
