d50c37e699c4a1db16f27682676e644b92df11c1
henrymazza <fabio.mazarotto@me.com>
2012-03-24 15:12:15 -0300

    Code to kill Resque child processes. Not working probably because actual version of Runit cookbook doesn't support notifications.

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -12,11 +12,20 @@ worker_count = 4
 apps = node.run_list.roles & search(:apps).map(&:id)
 
 apps.each do |app|
-
+  rvm_ruby = search(:apps, "id:#{app}").first['rvm_ruby']
   runit_service "#{app}-resque" do
-    options = { :app_dir => "/var/www/apps/#{app}/current" }  
     template_name "resque"
+    options({
+      :app_dir => "/var/www/apps/#{app}/current",
+      :app_ruby => rvm_ruby
+    })
   end
-
+  # execute "kill -QUIT -`pgrep -f 'runsv #{app['id']}-resque'` && true" do
+  # => the above is not trustable, the below one is a little harsh 
+  execute "pgrep -f resque | xargs -l sudo kill" do
+    user "root"
+    ignore_failure true
+    action :nothing
+    subscribes :restart, "service[#{app}-resque]"
+  end
 end
-
