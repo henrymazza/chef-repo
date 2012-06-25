d0a04d6c6386952f6f573e3baf021fc5e17d0df8
Fabio Mazarotto <fabio.mazarotto@me.com>
2012-06-25 16:36:34 -0300

    new vm name, update ruby version

--- vm/definitions/oneiric/postinstall.sh
+++ vm/definitions/oneiric/postinstall.sh
@@ -20,14 +20,14 @@ apt-get -y install nfs-common
 
 # Install Ruby from source in /opt so that users of Vagrant
 # can install their own Rubies using packages or however.
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
-tar xvzf ruby-1.9.3-p125.tar.gz
-cd ruby-1.9.3-p125
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
+tar xvzf ruby-1.9.2-p290.tar.gz
+cd ruby-1.9.2-p290
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.9.3-p125
+rm -rf ruby-1.9.2-p290
 
 # Install RubyGems 1.7.2
 wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.11.tgz
