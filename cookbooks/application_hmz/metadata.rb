668ce30ced3c38f330ecad5c2fb4071119bee2fa
Fabio Mazarotto <fabio.mazarotto@me.com>
2012-08-28 14:28:32 -0300

    application_hmz is being substituted by the new application/application_ruby cookbook

--- cookbooks/application_hmz/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Deploys and configures a variety of applications defined from databag 'apps'"
-
-version          "0.0.1"
-
-recipe           "application::rails", "Deploys a Rails application specified in a data bag with the deploy_revision resource"
-
