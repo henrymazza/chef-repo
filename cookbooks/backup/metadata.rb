9bd5e507290a193676b5f9a522282971023a6f04
Fabio Mazarotto <fabio.mazarotto@me.com>
2015-01-31 17:50:04 -0200

    Backup is deprecated. Removing.

--- cookbooks/backup/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Blissio"
-maintainer_email "alastair@blissio.com"
-license          "MIT"
-description      "Installs/Configures backup"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
-version          "0.0.1"
